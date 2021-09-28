883cbf0f10558243ad5164f30750d653fa5a6e09
lorneez <lornz17@gmail.com>
2021-09-28 17:24:34 -0400

    Added inventory class

---
+++ neural_mmo/forge/blade/item/item.py
@@ -0,0 +1,4 @@
+class Item:
+    def __init__(self, item_type, description):
+        self.item_type = item_type
+        self.description = description
--- neural_mmo/forge/blade/systems/inventory.py
+++ neural_mmo/forge/blade/systems/inventory.py
@@ -1,17 +1,33 @@
 from neural_mmo.forge.blade.item import item, armor
+from neural_mmo.forge.blade.item.item import Item
+from neural_mmo.forge.blade.systems.equipment import Armor
+
 
 class Inventory:
-   def __init__(self, config):
-      self.ammo = 0
-      #self.resetArmor()
-      #self.resetMelee()
-      #self.resetRanged()
+    def __init__(self, config):
+        # self.ammo = 0
+        self.items = []
+        # self.resetArmor()
+        # self.resetMelee()
+        # self.resetRanged()
 
-   def resetArmor(self):
-      self.armor = Armor.Base()
+    # def resetArmor(self):
+    #    self.armor = Armor.Base()
+    #
+    # def resetMelee(self):
+    #    self.melee = Item.Base()
+    #
+    # def resetRanged(self):
+    #    self.ranged = Item.Base()
 
-   def resetMelee(self):
-      self.melee = Item.Base()
-      
-   def resetRanged(self):
-      self.ranged = Item.Base()
+    def insertItem(self, item):
+        self.items.append(item)
+
+    def simplifiedInventory(self):
+        inv = dict()
+        for item_ele in self.items:
+            if item_ele.item_type in inv:
+                inv[item_ele.item_type] = inv[item_ele.item_type] + 1
+            else:
+                inv[item_ele.item_type] = 1
+        return inv
