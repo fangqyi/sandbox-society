d9a28b73ee31cd2a2b7b8fc1528d1dc8414ada91
lorneez <lornz17@gmail.com>
2021-10-07 18:10:40 -0400

    Added basic communication lights to player

--- neural_mmo/forge/blade/entity/player.py
+++ neural_mmo/forge/blade/entity/player.py
@@ -9,77 +9,106 @@ from neural_mmo.forge.blade.systems.achievement import Diary
 from neural_mmo.forge.blade.entity import entity
 from neural_mmo.forge.blade.io.stimulus import Static
 
+
 class Player(entity.Entity):
-   def __init__(self, realm, pos, agent):
-      super().__init__(realm, pos, agent.iden, agent.name, agent.color, agent.pop)
-      self.agent  = agent
-      self.pop    = agent.pop
+    def __init__(self, realm, pos, agent):
+        super().__init__(realm, pos, agent.iden, agent.name, agent.color, agent.pop)
+        self.agent = agent
+        self.pop = agent.pop
 
-      #Scripted hooks
-      self.target = None
-      self.food   = None
-      self.water  = None
-      self.vision = 7
+        # Communication light
+        self.communication_light = 0
 
-      #Submodules
-      self.skills       = Skills(self)
-      self.achievements = Diary(realm.config)
+        # Scripted hooks
+        self.target = None
+        self.food = None
+        self.water = None
+        self.vision = 7
 
-      self.dataframe.init(Static.Entity, self.entID, self.pos)
+        # Submodules
+        self.skills = Skills(self)
+        self.achievements = Diary(realm.config)
 
-   @property
-   def serial(self):
-      return self.population, self.entID
+        self.dataframe.init(Static.Entity, self.entID, self.pos)
 
-   @property
-   def isPlayer(self) -> bool:
-      return True
+    @property
+    def serial(self):
+        return self.population, self.entID
 
-   @property
-   def population(self):
-      return self.pop
+    @property
+    def isPlayer(self) -> bool:
+        return True
 
-   def applyDamage(self, dmg, style):
-      self.resources.food.increment(dmg)
-      self.resources.water.increment(dmg)
-      self.skills.applyDamage(dmg, style)
-      
-   def receiveDamage(self, source, dmg):
-      if not super().receiveDamage(source, dmg):
-         if source:
-            source.history.playerKills += 1
-         return 
+    @property
+    def population(self):
+        return self.pop
 
-      self.resources.food.decrement(dmg)
-      self.resources.water.decrement(dmg)
-      self.skills.receiveDamage(dmg)
+    def getLight(self):
+        if self.communication_light == 0:
+            return "OFF"
+        elif self.communication_light == 1:
+            return "RED"
+        elif self.communication_light == 2:
+            return "BLUE"
+        elif self.communication_light == 3:
+            return "GREEN"
+        else:
+            return "OFF"
 
-   def receiveLoot(self, loadout):
-      if loadout.chestplate.level > self.loadout.chestplate.level:
-         self.loadout.chestplate = loadout.chestplate
-      if loadout.platelegs.level > self.loadout.platelegs.level:
-         self.loadout.platelegs = loadout.platelegs
+    def setLight(self, color):
+        if color == "OFF":
+            self.communication_light = 0
+        elif color == "RED":
+            self.communication_light = 1
+        elif color == "BLUE":
+            self.communication_light = 2
+        elif color == "GREEN":
+            self.communication_light = 3
+        else:
+            self.communication_light = 0
 
-   def packet(self):
-      data = super().packet()
 
-      data['entID']    = self.entID
-      data['annID']    = self.population
+    def applyDamage(self, dmg, style):
+        self.resources.food.increment(dmg)
+        self.resources.water.increment(dmg)
+        self.skills.applyDamage(dmg, style)
 
-      data['base']     = self.base.packet()
-      data['resource'] = self.resources.packet()
-      data['skills']   = self.skills.packet()
+    def receiveDamage(self, source, dmg):
+        if not super().receiveDamage(source, dmg):
+            if source:
+                source.history.playerKills += 1
+            return
 
-      return data
-  
-   def update(self, realm, actions):
-      '''Post-action update. Do not include history'''
-      super().update(realm, actions)
+        self.resources.food.decrement(dmg)
+        self.resources.water.decrement(dmg)
+        self.skills.receiveDamage(dmg)
 
-      if not self.alive:
-         return
+    def receiveLoot(self, loadout):
+        if loadout.chestplate.level > self.loadout.chestplate.level:
+            self.loadout.chestplate = loadout.chestplate
+        if loadout.platelegs.level > self.loadout.platelegs.level:
+            self.loadout.platelegs = loadout.platelegs
 
-      self.resources.update(realm, self, actions)
-      self.skills.update(realm, self, actions)
-      self.achievements.update(realm, self)
-      #self.inventory.update(world, actions)
+    def packet(self):
+        data = super().packet()
+
+        data['entID'] = self.entID
+        data['annID'] = self.population
+
+        data['base'] = self.base.packet()
+        data['resource'] = self.resources.packet()
+        data['skills'] = self.skills.packet()
+
+        return data
+
+    def update(self, realm, actions):
+        '''Post-action update. Do not include history'''
+        super().update(realm, actions)
+
+        if not self.alive:
+            return
+
+        self.resources.update(realm, self, actions)
+        self.skills.update(realm, self, actions)
+        self.achievements.update(realm, self)
+        # self.inventory.update(world, actions)
