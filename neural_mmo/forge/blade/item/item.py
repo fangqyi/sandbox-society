8a53dc3a9d6c98c4856d6d7ec47e23d3f300888a
lorneez <lornz17@gmail.com>
2021-10-07 16:23:03 -0400

    Added checks for wood technology, refactored inventory storage, added helper methods

--- neural_mmo/forge/blade/item/item.py
+++ neural_mmo/forge/blade/item/item.py
@@ -1,4 +1,22 @@
+from enum import Enum
+
+
 class Item:
     def __init__(self, item_type, description):
         self.item_type = item_type
         self.description = description
+
+
+class ItemType(Enum):
+    RED_BERRY = 1
+    BLUE_BERRY = 2
+    YELLOW_BERRY = 3
+    DIRT = 4
+    SMALL_STICK = 5
+    LARGE_BRANCH = 6
+    SMALL_ROCK = 7
+    LARGE_BOULDER = 8
+    COAL = 9
+    IRON = 10
+    GOLD = 11
+    MEAT = 12
--- neural_mmo/forge/blade/systems/inventory.py
+++ neural_mmo/forge/blade/systems/inventory.py
@@ -1,33 +1,84 @@
 from neural_mmo.forge.blade.item import item, armor
-from neural_mmo.forge.blade.item.item import Item
+from neural_mmo.forge.blade.item.item import Item, ItemType
 from neural_mmo.forge.blade.systems.equipment import Armor
 
 
 class Inventory:
-    def __init__(self, config):
-        # self.ammo = 0
-        self.items = []
-        # self.resetArmor()
-        # self.resetMelee()
-        # self.resetRanged()
+    def __init__(self):
+        self.items = dict()
 
-    # def resetArmor(self):
-    #    self.armor = Armor.Base()
-    #
-    # def resetMelee(self):
-    #    self.melee = Item.Base()
-    #
-    # def resetRanged(self):
-    #    self.ranged = Item.Base()
+    def checkWoodTechnologyStatus(self):
+        amount = self.getItemAmount(ItemType.SMALL_STICK)
+        if amount >= 10:
+            return True
+        else:
+            return False
 
-    def insertItem(self, item):
-        self.items.append(item)
+    def getItemAmount(self, item_type):
+        if item_type not in self.items.keys():
+            return 0
+        else:
+            return self.items[item_type]
 
-    def simplifiedInventory(self):
-        inv = dict()
-        for item_ele in self.items:
-            if item_ele.item_type in inv:
-                inv[item_ele.item_type] = inv[item_ele.item_type] + 1
+    def hasItems(self, item_type, item_amount):
+        if self.items[item_type] >= item_amount:
+            return True
+        return False
+
+    def removeItems(self, item_type, item_amount):
+        if self.items[item_type] < item_amount:
+            print("not enough to remove")
+            return False
+        else:
+            for i in range(item_amount):
+                print("remove: ", item_type)
+                self.items[item_type] = self.items[item_type] - 1
+            return True
+
+    def insertItems(self, items_list):
+        for item_ele in items_list:
+            print("insert: ", item_ele.item_type)
+            if item_ele.item_type in self.items.keys():
+                self.items[item_ele.item_type] = self.items[item_ele.item_type] + 1
             else:
-                inv[item_ele.item_type] = 1
-        return inv
+                self.items[item_ele.item_type] = 1
+
+
+def runInventoryTest():
+    test_inv = Inventory()
+    test_inv.insertItems([
+        Item(ItemType.COAL, "lump of coal"),
+        Item(ItemType.COAL, "lump of coal"),
+        Item(ItemType.COAL, "lump of coal"),
+        Item(ItemType.COAL, "lump of coal")
+    ])
+    test_inv.insertItems([
+        Item(ItemType.GOLD, "gold"),
+        Item(ItemType.DIRT, "dirt"),
+        Item(ItemType.DIRT, "dirt"),
+        Item(ItemType.IRON, "iron")
+    ])
+    print("has 4 coal:", test_inv.hasItems(ItemType.COAL, 4))
+    print("has 6 coal:", test_inv.hasItems(ItemType.COAL, 6))
+    print(test_inv.removeItems(ItemType.COAL, 2))
+    print("has 4 coal:", test_inv.hasItems(ItemType.COAL, 4))
+    print("gold amount:", test_inv.getItemAmount(ItemType.GOLD))
+    print("small stick amount:", test_inv.getItemAmount(ItemType.SMALL_STICK))
+    print("wood tech status:", test_inv.checkWoodTechnologyStatus())
+    test_inv.insertItems([
+        Item(ItemType.SMALL_STICK, "small stick"),
+        Item(ItemType.SMALL_STICK, "small stick"),
+        Item(ItemType.SMALL_STICK, "small stick"),
+        Item(ItemType.SMALL_STICK, "small stick"),
+        Item(ItemType.SMALL_STICK, "small stick"),
+        Item(ItemType.SMALL_STICK, "small stick"),
+        Item(ItemType.SMALL_STICK, "small stick"),
+        Item(ItemType.SMALL_STICK, "small stick"),
+        Item(ItemType.SMALL_STICK, "small stick"),
+        Item(ItemType.SMALL_STICK, "small stick")
+    ])
+    print("small stick amount:", test_inv.getItemAmount(ItemType.SMALL_STICK))
+    print("wood tech status:", test_inv.checkWoodTechnologyStatus())
+
+
+runInventoryTest()
