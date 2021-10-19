f8c5feef0d2dd76d0128ebb7a0576e3d7b59c00a
lorneez <lornz17@gmail.com>
2021-10-19 18:04:42 -0400

    Added __call__ method to LightAgent

--- neural_mmo/forge/trinity/scripted/light_agent.py
+++ neural_mmo/forge/trinity/scripted/light_agent.py
@@ -1,8 +1,22 @@
 from neural_mmo.forge.trinity.scripted.baselines import Scripted
 from neural_mmo.forge.trinity.scripted import light_communication
-
+import random
 
 class LightAgent(Scripted):
+
+    def __call__(self, obs):
+        super().__call__(obs)
+
+        signals = [0, 1, 2]
+        choice = random.choice(signals)
+        if choice == 0:
+            self.signal_blue()
+        elif choice == 1:
+            self.signal_purple()
+        else:
+            self.signal_yellow()
+
+        return self.actions
 
     def signal_blue(self):
         light_communication.signal(self.config, self.actions, "BLUE")
